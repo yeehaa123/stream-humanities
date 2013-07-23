cc3bfdc7998cde4123ba001c9f4b55748a6c5afa
Yeehaa <medialoperations@gmail.com>
2013-07-22 18:03:22 -0700

    run it in foreground

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ TweetStream.configure do |config|
   config.auth_method        = :oauth
 end
 
-TweetStream::Daemon.new('tracker').sample do |status|
+TweetStream::Client.new.sample do |status|
   tweet = status.text.split(" ")
   triggers = %w{art job hiring new degree career teacher education dark age life code algorithm javascript ruby make future html css language data visualisation learning} 
   output = nil 
